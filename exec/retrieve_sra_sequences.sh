86382f855735e368e653ddf5602b1b969a1beb93
twumasianknad <twumasianknad@vcu.edu>
2020-08-31 00:51:12 -0400

    Fixed some tricky bugs

--- exec/retrieve_sra_sequences.sh
+++ exec/retrieve_sra_sequences.sh
@@ -1,28 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # A shell script to download fastq files from NCBI's SRA database
 # Written by: Nana Afia Twumasi-Ankrah
-# Last updated on: 03/20/2020
+# Last updated on: 08/30/2020
 # -------------------------------------------------------
 
+# ERROR CHECKS:
 # Verify the type of input and number of values
-# Display an error message if both inputs are not provided
+# Display an error message if no inputs are not provided
 # Exit the shell script with a status of 1 using exit 1 command.
 
+# Check to see if any arguments are provided
 [ $# -eq 0 ] && { echo "Usage: $0 path/to/fastq-dump path/to/SRR_Acc_List.txt path/to/output/"; exit 1; }
 
 
-fastq="fastq-dump" # Path to fastq-dump script (keep in same directory)
+fastq=$1 # Path to fastq-dump script (keep in same directory)
+[[ ! -f $fastq ]] && { echo "fastq-dump script could not be found (invalid path): $fastq"; exit 1; }
+
 input=$2 # Path to list of SRA ids
+[[ ! -f $input ]] && { echo "Input file could not found (invalid path): $input"; exit 1; }
+
 output=$3 # Path to output directory
 
 
-# Create output directory
-mkdir $output
+# Create output directory if it doesn't already exist
+if [ ! -d "$output" ]; then
+	read -p "Are you sure you want to create a new directory? <y/N> ";
+	if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+	then
+ 		echo "Creating new directory: $output";
+		mkdir $output;
+	else
+		echo "Unable to create new directory. Please enter new path.";
+  		exit 0;
+	fi
+fi
+
+# Navigate to output directory
 cd $output
 
 # Read list and install files
 while IFS= read -r line
 do
-  $fastq --split-3 --gzip "$line"
+	$fastq --split-3 --gzip "$line"
 done < "$input"
 
--- tests/testthat/test-getSeqs.R
+++ tests/testthat/test-getSeqs.R
@@ -1,23 +1,27 @@
 library(testthat)
 context("Download FASTQ sequences from NCBI SRA database")
 
-source("readConfig.R")
-source("setup.R")
-source("getSeqs.R")
+root <- "/gpfs_fs/vahmp/users/Fettweis/Atopobium_NTA/Wright_Collaboration/r_package/fastq2otu/"
+
+source(paste0(root, "R/readConfig.R"))
+source(paste0(root, "R/setup.R"))
+source(paste0(root, "R/getSeqs.R"))
 
 
-config <- #TODO
-options <- yaml::yaml.load_file(config)
+paired_config <- paste0(root, "inst/examples/paired/paired-example_config.yml")
+paired_options <- yaml::yaml.load_file(paired_config)
 
+# TODO: Add single-end tests
 
-test_that("Use wget to install files", {
-  object <- readConfig(config, type = "seqdump")
+# TODO: SRA-Explorer website is down (08/30/31) 
+#test_that("Use wget to install paired-end data files", {
+#  object <- readConfig(paired_config, type = "seqdump")
+#  fp <- getSeqs(object)
+#  expect_true(fp == paired_options$pathToData)
+#})	
+
+test_that("Use fastq-dump to install paired-end data files", {
+  object <- readConfig(paired_config, type = c("seqdump")) # TODO: Fix if-else statements so when invalid type combinations are provided a clear(er) error message is given
   fp <- getSeqs(object)
-  expect_true(fp == options$pathToData)
-})
-
-test_that("Use fastq-dump to install files", {
-  object <- readConfig(config, type = c("auto", "seqdump"))
-  fp <- getSeqs(object)
-  expect_true(fp == options$pathToData)
+  expect_true(fp == paired_options$pathToData)
 })
