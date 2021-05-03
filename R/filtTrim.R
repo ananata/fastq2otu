91081a9e1903254d6792d615d71c30605ceffd3e
ananata <twumasianknad@vcu.edu>
2021-05-03 11:30:33 -0400

    Updated filtTrim

--- R/filtTrim.R
+++ R/filtTrim.R
@@ -14,34 +14,43 @@ filtTrim <- function(object, sample.names, forwardFs = NA, reverseRs = NA) {
 	if (object@isPaired) {
 		# Create output file names for filtered sequences
 		filtFs <- file.path(path, paste0(label, "_filtered"), paste0(sample.names, "_R1_filt_trimmed.fastq.gz"))
+		undownloaded_filtFs <- filtFs[lapply(filtFs, file.exists) == FALSE]
+		forwardFs <- forwardFs[undownloaded_filtFs]
 		filtRs <- file.path(path, paste0(label, "_filtered"), paste0(sample.names, "_R2_filt_trimmed.fastq.gz"))
+		undownloaded_filtRs <- filtRs[lapply(filtRs, file.exists) == FALSE]
+		reverseRs <- reverseRs[undownloaded_filtRs]
+		if (length(undownloaded_filtFs) != 0) { 
+			# Filter and Trim (only executes paths that do not already exist)
+			filt_trim <- dada2::filterAndTrim(fwd=forwardFs, filt=undownloaded_filtFs, rev=reverseRs, filt.rev=undownloaded_filtRs, 
+					maxEE=as.vector(object@filtMaxEE), trimLeft=as.vector(object@filtTrimLeft), trimRight=as.vector(object@filtTrimRight), 
+					truncLen=as.vector(object@filtTruncLen), multithread=object@filtMultiThread,verbose=object@filtVerbose, 
+					minLen=as.vector(object@filtMinLen), matchIDs=object@filtMatchIDs, truncQ=as.vector(object@filtTruncQ),
+					compress=TRUE)
 		
-		# Filter and Trim (only executes paths that do not already exist)
-		filt_trim <- dada2::filterAndTrim(fwd=forwardFs, filt=filtFs, rev=reverseRs, filt.rev=filtRs, maxEE=as.vector(object@filtMaxEE), trimLeft=as.vector(object@filtTrimLeft),
-								trimRight=as.vector(object@filtTrimRight), truncLen=as.vector(object@filtTruncLen), multithread=object@filtMultiThread,
-								verbose=object@filtVerbose, minLen=as.vector(object@filtMinLen), matchIDs=object@filtMatchIDs, truncQ=as.vector(object@filtTruncQ),
-								compress=TRUE)
-		
-		# Save important R objects (needed for summary table)
-		saveFilt <- filt_trim
-		save(saveFilt, file = file.path(path, "filtered_objects.RData"))
-		
-		# Return both paths
-		return(c(filtFs, filtRs))
-								
+			# Save important R objects (needed for summary table)
+			saveFilt <- filt_trim
+			save(saveFilt, file = file.path(path, "filtered_objects.RData"))
+			
+			# Return both paths
+			return(c(filtFs, filtRs))
+		}			
 
 	} else {		
 		# Create output file names for filtered sequences
 		filtFs <- file.path(path, paste0(label, "_filtered"), paste0(sample.names, "_filt_trimmed.fastq"))
+		undownloaded_filtFs <- filtFs[lapply(filtFs, file.exists) == FALSE]
+		forwardFs <- forwardFs[undownloaded_filtFs]
 
-		# Filter and Trim (only executes filtered paths that do not already exist)
-		filtTrim <- dada2::filterAndTrim(fwd=forwardFs, filt=filtFs, maxEE=object@filtMaxEE, trimLeft=object@filtTrimLeft,
+		if (length(undownloaded_filtFs) != 0) {
+			# Filter and Trim (only executes filtered paths that do not already exist)
+			filtTrim <- dada2::filterAndTrim(fwd=forwardFs, filt=undownloaded_filtFs, maxEE=object@filtMaxEE, trimLeft=object@filtTrimLeft,
 								trimRight=object@filtTrimRight, truncLen=object@filtTruncLen, multithread=object@filtMultiThread,
 								verbose=object@filtVerbose, minLen=object@filtMinLen, matchIDs=object@filtMatchIDs, truncQ=object@filtTruncQ)
 		
-		# Save important R objects (needed for summary table)
-		saveFilt <- filtTrim
-		save(saveFilt, file = file.path(path, "filtered_objects.RData"))
+			# Save important R objects (needed for summary table)
+			saveFilt <- filtTrim
+			save(saveFilt, file = file.path(path, "filtered_objects.RData"))
+		}
 	}
 
 	# Return paths
